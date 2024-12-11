8b3becda17802ffc7236f26e4809291ae73bb3cc
Jennifer087 <jenniferhuynh087@gmail.com>
2024-12-11 21:47:33 +0000

    compiled stuff

--- classwork.cpp
+++ classwork.cpp
@@ -3,11 +3,15 @@
 
 int main()
 {
-
-  int age;
-  age = 10;
+  //SET age TO 10
+  int age = 10;
   double price = 19.99;
-  int grade;
-  grade = 11;
+  bool hasPets = true;
+  
+  std::cout << age << "\n" << price << "\n" << hasPets << "\n";
+
+  std::cout << "I am " << age << " years old. \n";
+
+  std::cout << "The price is " << price << " it's really cheap. \n ";
   return 0;
 }
