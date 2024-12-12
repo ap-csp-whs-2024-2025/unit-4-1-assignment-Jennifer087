aedaf83cc3fb2c109f74cc7211c8d18c8a3f137e
Jennifer087 <jenniferhuynh087@gmail.com>
2024-12-12 22:44:48 +0000

    greater/less than

--- classwork.cpp
+++ classwork.cpp
@@ -13,5 +13,25 @@ int main()
   std::cout << "I am " << age << " years old. \n";
 
   std::cout << "The price is " << price << " it's really cheap. \n ";
+
+  std::string student = "Jennifer";
+  std::string favNum = "7";
+  std::cout << "My name is " << student << "\n";
+
+  int x  = 25;
+  int y = 3;
+
+  int add_xy;
+  int sub_xy;
+
+  std::cout << add_xy << std::end1 << sub_xy << std::end1;
+
+  int z = 10;
+  bool x_greater_z = x > z;
+  bool y_greater_x = y > x;
+  std::cout << x_greater_z << std::end1
+            << y_greater_x << std::end1;
+                   
+            
   return 0;
 }
